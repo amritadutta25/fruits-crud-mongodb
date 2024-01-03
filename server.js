9a4ab658218614a94b37a45be3d657faf7f5f2fe
Alex Merced <alex@alexmerced.dev>
2024-01-03 18:56:35 -0500

    moved models in models folder

--- server.js
+++ server.js
@@ -6,45 +6,7 @@ const express = require("express"); // our web framework
 const morgan = require("morgan"); // our logger
 const methodOverride = require("method-override"); // override forms
 const mongoose = require("mongoose"); // connect to our mongodb
-
-////////////////////////////////////
-// DATABASE CONNECTION
-////////////////////////////////////
-// our database connection string
-const DATABASE_URL = process.env.DATABASE_URL;
-
-// establish our connection
-mongoose.connect(DATABASE_URL);
-
-// Events for when the connection changes
-mongoose.connection
-  .on("open", () => {
-    console.log("connected to Mongo");
-  })
-  .on("close", () => {
-    console.log("Disconnected from Mongo");
-  })
-  .on("error", (error) => {
-    console.log(error);
-  });
-
-/////////////////////////////////////////
-// Create Our Fruits Model
-/////////////////////////////////////////
-// destructure Schema and model into their own variables
-const { Schema, model } = mongoose;
-// const Schema = mongoose.Schema
-// const model = mongoose.model
-
-// Schema - Shape of the Data
-const fruitSchema = new Schema({
-  name: String,
-  color: String,
-  readyToEat: Boolean,
-});
-
-// Model - object for interacting with the db
-const Fruit = model("Fruit", fruitSchema);
+const Fruit = require("./models/Fruit")
 
 //////////////////////////////////////////////////
 // Express App Object
