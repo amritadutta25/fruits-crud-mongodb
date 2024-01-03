d1774301c8e578e93a1a9d6b05f08450c43ff9fa
Alex Merced <alex@alexmerced.dev>
2024-01-02 21:36:54 -0500

    new and create

--- scratch.js
+++ scratch.js
@@ -1,10 +1,24 @@
-console.log(process)
+// console.log(process)
 
-try{
+// try{
 
-}catch(error){
-    console.log("-----", error.message, "------")
-    res.status(400).send("error, read logs for details")
-}
+// }catch(error){
+//     console.log("-----", error.message, "------")
+//     res.status(400).send("error, read logs for details")
+// }
 
 // expression ? true : false
+
+
+// asyncAct1.then((thing1) => {
+//     asyncAct2().then((thing2) => {
+
+//     })
+// })
+
+// const thing1 = await asyncAct1()
+// const thing2 = await asyncAct12()
+
+
+const cheese = 1 > 2 ? "gouda" : (1 === 1 ? "munster" : "cheddar")
+console.log(cheese)
--- server.js
+++ server.js
@@ -98,7 +98,28 @@ app.get("/fruits", async (req, res) => {
     const fruits = await Fruit.find({});
     // render a template
     // fruits/index.ejs = ./views/fruits/index.ejs
-    res.render("fruits/index.ejs", {fruits})
+    res.render("fruits/index.ejs", { fruits });
+  } catch (error) {
+    console.log("-----", error.message, "------");
+    res.status(400).send("error, read logs for details");
+  }
+});
+
+// New Route
+app.get("/fruits/new", (req, res) => {
+  res.render("fruits/new.ejs");
+});
+
+// Create Route (Post to /fruits)
+app.post("/fruits", async (req, res) => {
+  try {
+    // check if readyToEat should be true
+    // expression ? true : false (ternary operator)
+    req.body.readyToEat = req.body.readyToEat === "on" ? true : false;
+    // create the fruit in the database
+    await Fruit.create(req.body);
+    // redirect back to main page
+    res.redirect("/fruits");
   } catch (error) {
     console.log("-----", error.message, "------");
     res.status(400).send("error, read logs for details");
@@ -107,20 +128,20 @@ app.get("/fruits", async (req, res) => {
 
 // The Show Route (Get to /fruits/:id)
 app.get("/fruits/:id", async (req, res) => {
-    try{
-        // get the id from params
-        const id = req.params.id
+  try {
+    // get the id from params
+    const id = req.params.id;
 
-        // find the particular fruit from the database
-        const fruit = await Fruit.findById(id)
+    // find the particular fruit from the database
+    const fruit = await Fruit.findById(id);
 
-        // render the template with the fruit
-        res.render("fruits/show.ejs", {fruit})
-    }catch(error){
-        console.log("-----", error.message, "------")
-        res.status(400).send("error, read logs for details")
-    }
-})
+    // render the template with the fruit
+    res.render("fruits/show.ejs", { fruit });
+  } catch (error) {
+    console.log("-----", error.message, "------");
+    res.status(400).send("error, read logs for details");
+  }
+});
 
 ///////////////////////////////////////////////////////
 // Server Listener
